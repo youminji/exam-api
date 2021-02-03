b561cb5e45d666c5975d8eb731d7bf7ad2d350a5
youminji62 <youminji62@gmail.com>
2021-02-03 16:50:33 +0900

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello, Nodejs!'));
+app.get('/welcome', (req, res) => {
+    var status = req.param('on');
+    res.send('The system is running [Status:' + status + ']');
+});
+
+app.listen(port, () => console.log('Example app listening on port 3000'))
